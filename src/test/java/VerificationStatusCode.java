92ecbb381b17f3d77d56794fff451aa0d2e2284e
TatianaVyhranenko <tanchik181994@gmail.com>
2022-02-20 18:25:20 +0200

    Created test.

---
+++ src/test/java/VerificationStatusCode.java
@@ -0,0 +1,22 @@
+import org.apache.http.StatusLine;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class VerificationStatusCode {
+
+    private final static String URL = "https://reqres.in/";
+    private final int EXPECTED_STATUS_CODE = 404;
+
+    @Test
+    public void checkStatusCode() {
+        int actualStatusCode = given()
+                .when()
+                .get(URL + "/api/users/23")
+                .getStatusCode();
+
+        Assert.assertNotNull(actualStatusCode);
+        Assert.assertEquals(actualStatusCode, EXPECTED_STATUS_CODE);
+    }
+}
